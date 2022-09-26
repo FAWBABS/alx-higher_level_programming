d844acfdb71d4a0ec6d81fa646503c40f55ce04f
FAWBABS <fawazbabaodutayo@gmail.com>
2022-09-26 17:44:03 +0000

    magic

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
+
+def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
 
